10f168480a6d238bf97934cb13d57cb610fa8a53
Moses Tenkorang <mbtenkorang@yahoo.com>
2024-02-27 11:09:38 +0000

    Improve accessibility of <button> elements

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,13 @@
             class="text-base md:text-lg text-pretty font-semibold text-dark-purple hover:text-hover-color hover:cursor-pointer focus:text-gray-purple w-10/12">
             {{ faq.faqQuestion }}</h2>
           <div>
-            <button v-if="activeIndex === index"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="31"
-                fill="none" viewBox="0 0 30 31">
+            <button v-if="activeIndex === index" type="button" name="show answer" aria-pressed="false"><svg
+                xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31">
                 <path fill="#301534"
                   d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z" />
               </svg></button>
-            <button v-else><svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31">
+            <button v-else type="button" name="hide answer">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31">
                 <path fill="#AD28EB"
                   d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z" />
               </svg>
